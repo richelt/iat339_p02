2b5efc89593d18ad4a763801290908d87eb9989e
Marya Smirnova <msmirnov@sfu.ca>
2015-06-25 23:41:39 -0700

    making changes in progressss

--- js/extra.js
+++ js/extra.js
@@ -1,18 +1,18 @@
-$(function(){
-    var lastScrollTop = 0, delta = 5;
-    $(window).scroll(function(event){
-       var st = $(this).scrollTop();
+// $(function(){
+//     var lastScrollTop = 0, delta = 5;
+//     $(window).scroll(function(event){
+//        var st = $(this).scrollTop();
        
-       if(Math.abs(lastScrollTop - st) <= delta)
-          return;
+//        if(Math.abs(lastScrollTop - st) <= delta)
+//           return;
        
-       if (st > lastScrollTop){
-         // Scroll down
-         $('header').addClass('nav-hide');
-       } else {
-         // Scroll up
-         $('header').removeClass('nav-hide');
-       }
-       lastScrollTop = st;
-    });
-});
+//        if (st > lastScrollTop){
+//          // Scroll down
+//          $('header').addClass('nav-hide');
+//        } else {
+//          // Scroll up
+//          $('header').removeClass('nav-hide');
+//        }
+//        lastScrollTop = st;
+//     });
+// });
