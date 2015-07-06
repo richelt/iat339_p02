4a8f60bed031f5780440df7b77b2620467da8408
Marya Smirnova <msmirnov@sfu.ca>
2015-07-06 00:05:46 -0700

    added new files

---
+++ js/extra.js
@@ -0,0 +1,43 @@
+$('.button-nav-side').click(function(){
+	if($('#sidebartoggler').is(':checked')){
+		$('#sidebartoggler').prop('checked', false); 
+	}
+
+	else {
+		$('#sidebartoggler').prop('checked', true);
+	}
+});
+
+
+function change(id){
+ if ($('#'+id).is(':checked')) {
+    $('#'+id +"label").html('X');
+  }else{
+    $('#'+id+ "label").html('MENU');
+  }
+};
+
+
+
+$(document).ready(function() {
+  
+  $("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+    
+  $("a.transition").click(function(event){
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(1000, redirectPage);    
+  });
+    
+  function redirectPage() {
+    window.location = linkLocation;
+  }
+  
+});
+
+
+$('#selected').click(function(){
+    $("figure.events img").css('opacity','0');
+});
